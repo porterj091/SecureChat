21d85c81fa671c9ba67507a4d08dd0e55c1e1c4d
Joseph Porter <joporter5606@live.com>
2017-04-13 15:20:59 -0700

    Got the rsa key to transfer over

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 https://vcansimplify.wordpress.com/2013/03/14/c-socket-tutorial-echo-server/
 https://shanetully.com/2012/04/simple-public-key-encryption-with-rsa-and-openssl/
+http://www.codepool.biz/how-to-use-openssl-generate-rsa-keys-cc.html
+http://stackoverflow.com/questions/12107204/send-rsa-public-key-by-socket
--- SecureChat.c
+++ SecureChat.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <stdint.h>
 #include <unistd.h>
+#include <signal.h>
 #include <arpa/inet.h>
 #include <openssl/aes.h>
 #include <openssl/rsa.h>
@@ -12,13 +13,32 @@
 
 #define SERVER_PORT 6993
 #define MESSAGE_SIZE 1024
+static int listen_fd, comm_fd, sockfd;
 
 void Usage();
 void runServer();
 void runClient(char *);
+void generateKeys(char *, char *);
+
+void sig_handler(int signo)
+{
+  if (signo == SIGINT)
+  {
+    printf("\n%s\n", "Shutting Down!");
+    close(listen_fd);
+    close(comm_fd);
+    close(sockfd);
+    exit(1);
+  }
+}
 
 int main(int argc, char *argv[])
 {
+
+  if(signal(SIGINT, sig_handler) == SIG_ERR)
+  {
+    printf("%s\n", "Can't catch SIGINT");
+  }
 
   if (argc > 3)
   {
@@ -54,8 +74,8 @@ int main(int argc, char *argv[])
 void runServer()
 {
   char message[MESSAGE_SIZE];
+  unsigned char rsaMessage[4096];
   char sendMessage[MESSAGE_SIZE];
-  int listen_fd, comm_fd;
 
   struct sockaddr_in serverAddr;
 
@@ -83,39 +103,46 @@ void runServer()
 
   comm_fd = accept(listen_fd, (struct sockaddr*) NULL, NULL);
 
+  // Read the RSA public key from the client
+  read(comm_fd, rsaMessage, 4096);
+
+  RSA *publicKey = NULL;
+  BIO *pub = BIO_new_mem_buf(rsaMessage, -1);
+  PEM_read_bio_RSAPublicKey(pub, &publicKey, NULL, NULL);
+  if(publicKey == NULL)
+  {
+    fprintf(stderr, "%s\n", "Problem reading public key from client");
+    close(comm_fd);
+    exit(1);
+  }
+  printf("%s\n", rsaMessage);
+
+  char* c = "1234567890123456";
+  int encryptedLen;
+  if((encryptedLen = RSA_public_encrypt(strlen(c), (unsigned char*)c, (unsigned char*)sendMessage, publicKey, RSA_PKCS1_PADDING)) == -1)
+  {
+    fprintf(stderr, "%s\n", "Error encrypting session key");
+    close(comm_fd);
+    exit(1);
+  }
+  write(comm_fd, sendMessage, strlen(sendMessage));
+
   while(1)
   {
     memset(&message, 0, sizeof(message));
     read(comm_fd, message, MESSAGE_SIZE);
 
-    int length = strlen(message);
-    // Grab the type of message INIT for when client is setting up connection
-    // NORMAL for just a message should be last byte in message
-    uint8_t type = (uint8_t)message[length-1];
-
-    if (type == 0x01) // Initiation message
-    {
-      printf("%s\n", "Client trying to connect!!\nEstablishing secure connection!!");
-      strcpy(sendMessage, "1234567890123456");
-      write(comm_fd, sendMessage, strlen(sendMessage) + 1);
-    }
-    else if (type == 0x02)  // Normal message
-    {
-      printf("Message Received: %s\n", message);
-    }
-    else
-    {
-      fprintf(stderr, "%s\n", "Message had formatting errors");
-      exit(1);
-    }
+    printf("Message Received: %s\n", message);
   }
+
+  close(comm_fd);
 }
 
 void runClient(char *ipAddr)
 {
-  int sockfd;
-  char sendline[MESSAGE_SIZE];
-  char recvline[MESSAGE_SIZE];
+  unsigned char sendline[MESSAGE_SIZE];
+  unsigned char recvline[MESSAGE_SIZE];
+  unsigned char RSALine[4096];
   struct sockaddr_in serverAddr;
 
   sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -142,22 +169,81 @@ void runClient(char *ipAddr)
     perror("CONNECTION");
     exit(1);
   }
-  printf("%s%d\n", "Got this far: ", __LINE__);
+  printf("%s\n", "Establishing a secure connection!");
+
+  char publicKey[4096];
+  char privateKey[4096];
+  generateKeys(publicKey, privateKey);
+
+  memset(&RSALine, 0, sizeof(RSALine));
+  memset(&recvline, 0, sizeof(recvline));
+  memcpy(RSALine, publicKey, 4096);
+
+  // Send the public RSA key to the server
+  write(sockfd, RSALine, 4096);
+
+  // What is send back is the encrypted AES session key
+  if(read(sockfd, recvline, MESSAGE_SIZE) != 16)
+  {
+      fprintf(stderr, "%s\n", "Problem with Key transfer");
+  }
+  printf("%s\n", recvline);
+
 
   while(1)
   {
     memset(&sendline, 0, sizeof(sendline));
     memset(&recvline, 0, sizeof(recvline));
-    fgets(sendline, MESSAGE_SIZE-1, stdin);
-    sendline[strlen(sendline)] = 0x01;
-    write(sockfd, sendline, strlen(sendline) + 1);
-    read(sockfd, recvline, MESSAGE_SIZE);
+    printf("%s", "Enter Message: ");
+    fgets((char*)sendline, MESSAGE_SIZE-1, stdin);
+    write(sockfd, sendline, strlen((char*)sendline));
     printf("%s\n", recvline);
   }
+
+  close(sockfd);
 }
 
 void Usage()
 {
   printf("%s\n", "Incorrect Usage!");
   printf("%s\n", "./SecureChat {-l | -c TARGET-IP}");
+}
+
+void generateKeys(char *publicDest, char *privateDest)
+{
+  RSA *keypair = NULL;
+  keypair = RSA_new();
+  BIGNUM *e = NULL;
+  e = BN_new();
+  BN_set_word(e, 65537);
+
+  if(RSA_generate_key_ex(keypair, 4096, e, NULL) != 1)
+  {
+    perror("RSA Generation");
+    exit(1);
+  }
+
+  BN_free(e);
+  e = NULL;
+
+  BIO *pri = BIO_new(BIO_s_mem());
+  BIO *pub = BIO_new(BIO_s_mem());
+
+  PEM_write_bio_RSAPrivateKey(pri, keypair, NULL, NULL, 0, NULL, NULL);
+  PEM_write_bio_RSAPublicKey(pub, keypair);
+
+  size_t pri_len = BIO_pending(pri);
+  size_t pub_len = BIO_pending(pub);
+
+  char *pri_key = malloc(pri_len + 1);
+  char *pub_key = malloc(pub_len + 1);
+
+  BIO_read(pri, pri_key, pri_len);
+  BIO_read(pub, pub_key, pub_len);
+
+  memcpy(publicDest, pub_key, pub_len);
+  memcpy(privateDest, pri_key, pri_len);
+
+  return;
+
 }
